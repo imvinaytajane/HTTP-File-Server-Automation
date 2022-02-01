b37fe44c8156b8c862516178c288a6181e5023c6
Vinay <imvinaytajane@gmail.com>
2022-02-01 19:14:33 +0530

    added some comments

--- http_server.py
+++ http_server.py
@@ -1,7 +1,12 @@
+# By Vinay Tajane on 1st Feb 2022
+
 import os
 import webbrowser
+
+# opening cmd executing command and closing cmd
 os.system("start /B start cmd.exe @cmd /c ipconfig & ipconfig>%userprofile%\HTTP-Server\ip.txt")
 
+# Searching in file for Default gateway
 file = open('ip.txt')
 for line in file:
     line = line.strip()
